ff07c93f3bb77b2737d3576de725552a3ac0a088
“Emmett <emmett.wechsler11@gmail.com>
2022-11-23 14:10:07 -0500

    Range

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -1,7 +1,16 @@
-def dup
-    
+def rec_range(start, finish)
+    return [] if finish < start
+    return [finish] if finish == start
+
+    final = rec_range(start, finish - 1)
+
+    final << start
 end
 
-def range
-
+def it_range(start, finish)
+    return [] if finish < start
+    (start..finish).map { |ele| ele }
 end
+
+# p it_range(1, 5)
+# p rec_range(1, 5)
